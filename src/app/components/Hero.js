5df8e33311f6240bc94e328db7673a0ac5a1dc92
Anvesh Kumar <anvkup2002@gmail.com>
2024-10-19 00:33:15 +0530

    hero about fixed

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -16,7 +16,7 @@ function Hero() {
                             </svg>
                         </a>
                         <a href="/about" class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
-                            Learn more
+                            About Us
                         </a>
                     </div>
                 </div>
