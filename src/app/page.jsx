6d617385ee609d16de76744c0436837348a37418
Anvesh Kumar <anvkup2002@gmail.com>
2024-10-19 09:21:38 +0530

    Meta Desc'n Updated, hero sec updated, card added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
 export default function Home() {
   return (
     <div className="">
-    <Navbar/>
-    <Hero/>
-    <Footer/>
+      <Navbar />
+      <Hero />
+      <Card/>
+      <Footer />
     </div>
   );
 }
