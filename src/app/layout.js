4372c9096f587b0ef86f6d9804fb9bef88eb668d
Anvesh Kumar <55266595+anvkup@users.noreply.github.com>
2025-05-20 05:20:15 +0000

    removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,41 +26,10 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        {/* Google Tag Manager Scripts */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-KK38CLHRMG"
-          strategy="afterInteractive" // Loads the script after page is interactive
-          async
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
 
-            gtag('config', 'G-KK38CLHRMG');
-          `}
-        </Script>
-        {/* <!-- Google Tag Manager (noscript) --> */}
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M7PGTCHS"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        {/* <!-- End Google Tag Manager (noscript) --> */}
       </body>
       <head>
         <meta name="google-site-verification" content="6OahjAwwsChOQROOfrq8ESlkcuzYi92k58NKLq9j3Zk" />
-        {/* Google Tag Manager Script */}
-        {/* <!-- Google Tag Manager --> */}
-        <Script id="gtm-init" strategy="afterInteractive">
-          {`
-      (function(w,d,s,l,i){w[l]=w[l]||[];
-        w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-        var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-        j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-        f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-M7PGTCHS');
-    `}
-        </Script>
       </head>
     </html>
   );
