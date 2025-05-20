5ef1f90d679c709611ee9ccf49ccf914590ad3dd
Anvesh Kumar <55266595+anvkup@users.noreply.github.com>
2025-05-20 05:06:20 +0000

    geotag

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,10 +27,10 @@ export default function RootLayout({ children }) {
       >
         {children}
         {/* Google Tag Manager Scripts */}
-        <Script 
+        <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-KK38CLHRMG"
           strategy="afterInteractive" // Loads the script after page is interactive
-          async 
+          async
         />
         <Script id="gtag-init" strategy="afterInteractive">
           {`
@@ -41,9 +41,23 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-KK38CLHRMG');
           `}
         </Script>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M7PGTCHS"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
       </body>
       <head>
         <meta name="google-site-verification" content="6OahjAwwsChOQROOfrq8ESlkcuzYi92k58NKLq9j3Zk" />
+        {/* Google Tag Manager Script */}
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M7PGTCHS');
+          `}
+        </Script>
       </head>
     </html>
   );
