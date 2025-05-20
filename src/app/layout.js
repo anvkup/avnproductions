df4c04d1ff59c99f78649c9dd2404487869d5eb2
Anvesh Kumar <55266595+anvkup@users.noreply.github.com>
2025-05-20 05:18:43 +0000

    tet

--- src/app/layout.js
+++ src/app/layout.js
@@ -49,14 +49,17 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="google-site-verification" content="6OahjAwwsChOQROOfrq8ESlkcuzYi92k58NKLq9j3Zk" />
         {/* Google Tag Manager Script */}
+        {/* <!-- Google Tag Manager --> */}
         <Script id="gtm-init" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-M7PGTCHS');
-          `}
+      (function(w,d,s,l,i){w[l]=w[l]||[];
+        w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-M7PGTCHS');
+    `}
         </Script>
       </head>
     </html>
